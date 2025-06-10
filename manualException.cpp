5b2620aab0aee080c6130038d13be308d327ab52
danishanr <danishaanr@gmail.com>
2025-06-10 19:17:50 +0700

    Menghapus baris pada program

--- manualException.cpp
+++ manualException.cpp
@@ -7,8 +7,6 @@ int main()
         cout << "Selamat Belajar di Prodi TI UMY" << endl;
         throw 0.5; // melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan dieksekusi" << endl;
-
-
     }
     catch (int a) {
         // blok ini akan dieksekusi
